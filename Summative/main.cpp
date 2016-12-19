495f5b10530f798159f21f262f65236a3b6fcaf8
Sam Faulkner <samfaulkner@Sams-MacBook-Air.local>
2016-12-19 17:06:02 -0500

    Initial Commit

---
+++ Summative/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Summative
+//
+//  Created by Sam Faulkner on 2016-12-19.
+//  Copyright © 2016 Sam Faulkner and Arsheya Jain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
